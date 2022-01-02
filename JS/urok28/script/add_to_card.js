06d898ae8de41baf2debda06237f249c7d295a9d
Alinochka07 <alina011988@icloud.com>
2022-01-02 20:15:19 +0900

    homework for lesson29 added

--- JS/urok28/script/add_to_card.js
+++ JS/urok28/script/add_to_card.js
@@ -1,2 +1,11 @@
 var product_title = document.getElementsByClassName("title");
 console.log(product_title[0].innerText);
+
+var product_image = document.getElementById("animal_print");
+console.log(product_image.src);
+
+var product_price = document.getElementById("animal_print_price");
+console.log(product_price.innerText);
+
+var product_old_price = document.getElementsByTagName("del")[0];
+console.log(product_old_price.innerText);
