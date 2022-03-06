e254a1ca12d96f7e529775799abb93a2bba396cc
Alinochka07 <alina011988@icloud.com>
2022-03-06 22:48:32 +0900

    changes in lesson 48

--- JS/urok48/script/lessons_tasks.js
+++ JS/urok48/script/lessons_tasks.js
@@ -124,7 +124,129 @@ console.log(newObj); // при создании переменной, JSON не 
 newObj.hobbies.hobby1 = "learning English";
 console.log(newObj); // новое значение ему передастся.
 
-}());
+
+
+
+
+
+// let room = {
+//     number: 23
+// };
+  
+// let meetup = {
+// title: "Conference",
+// participants: ["john", "ann"]
+// };
+  
+// meetup.place = room;       // meetup ссылается на room
+// room.occupiedBy = meetup; // room ссылается на meetup
+
+  
+// console.log(JSON.stringify(meetup));  // Ошибка: Преобразование цикличной структуры в JSON
+
+
+
+
+
+// let room = {
+//     number: 23
+//   };
+  
+//   let meetup = {
+//     title: "Conference",
+//     participants: [{name: "John"}, {name: "Alice"}],
+//     place: room // meetup ссылается на room
+//   };
+  
+//   room.occupiedBy = meetup; // room ссылается на meetup
+  
+//   console.log( JSON.stringify(meetup, ['title', 'participants', 'place', 'name', 'number']) );
+//   // {"title":"Conference","participants":[{},{}]}
+
+
+
+
+//   let user = {
+//     name: "John",
+//     age: 25,
+//     roles: {
+//       isAdmin: false,
+//       isEditor: true
+//     }
+//   };
+  
+//   console.log(JSON.stringify(user, null, 5)); // Параметр "space" применяется для логирования и красивого вывода.
+
+
+
+
+// let room = {
+//     number: 23,
+//     toJSON() {
+//       return this.number;
+//     }
+//   };
+  
+//   let meetup = {
+//     title: "Conference",
+//     room
+//   };
+  
+//   console.log( JSON.stringify(room) ); // 23
+  
+//   console.log( JSON.stringify(meetup) ); // {"title":"Conference","room":23}
+
+
+
+
+
+
+//   let room = {
+//     number: 23
+//   };
+  
+//   let meetup = {
+//     title: "Conference",
+//     date: new Date(Date.UTC(2017, 0, 1)),
+//     room
+//   };
+  
+//   console.log( JSON.stringify(meetup) );
+  
+    // {
+    //   "title":"Conference",
+    //   "date":"2017-01-01T00:00:00.000Z",  // (1)
+    //   "room": {"number":23}               // (2)
+    // }
+  
+
+
+
+// parse
+
+// let str = '{"title":"Conference","date":"2017-11-30T12:00:00.000Z"}';
+
+// let meetup = JSON.parse(str);
+
+// alert( meetup.date.getDate() ); // Error! он не будет знать, что "date" - это дата и не сможет конвертировать.
+
+
+
+
+
+
+// parse - но с преобразованием в настоящую дату.
+
+// let str = '{"title":"Conference","date":"2017-11-30T12:00:00.000Z"}';
+
+// let meetup = JSON.parse(str, function(key, value) {
+//   if (key == 'date') return new Date(value);
+//   return value;
+// });
+
+// alert( meetup.date.getDate() ); // 30 - теперь работает!
+
+// }());
 
 
 
