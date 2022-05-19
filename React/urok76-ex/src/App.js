01f7b5a9d29185dc00d5069e90ecc2a6418229f3
Alinochka07 <alina011988@icloud.com>
2022-05-19 12:12:11 +0900

    lifecycle components

---
+++ React/urok76-ex/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Test from './text';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Test argument={14}/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
